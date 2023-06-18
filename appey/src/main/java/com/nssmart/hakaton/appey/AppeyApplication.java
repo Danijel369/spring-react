13480d901e3fceed951c76f41d85b4fc9fd12f07
Danijel-Hodzic <110568027+Danijel369@users.noreply.github.com>
2023-06-18 11:35:07 +0000

    spring project initial setup

---
+++ appey/src/main/java/com/nssmart/hakaton/appey/AppeyApplication.java
@@ -0,0 +1,13 @@
+package com.nssmart.hakaton.appey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppeyApplication.class, args);
+	}
+
+}
---
+++ appey/src/test/java/com/nssmart/hakaton/appey/AppeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nssmart.hakaton.appey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
