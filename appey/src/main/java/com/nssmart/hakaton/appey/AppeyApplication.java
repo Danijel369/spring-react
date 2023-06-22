06219aa1fcecade7361007c183ef9571ed9e7b26
Danijel-Hodzic <110568027+Danijel369@users.noreply.github.com>
2023-06-22 14:03:58 +0000

    test commit

--- appey/src/main/java/com/nssmart/hakaton/appey/AppeyApplication.java
+++ appey/src/main/java/com/nssmart/hakaton/appey/AppeyApplication.java
@@ -2,8 +2,10 @@ package com.nssmart.hakaton.appey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class AppeyApplication {
 
 	public static void main(String[] args) {
